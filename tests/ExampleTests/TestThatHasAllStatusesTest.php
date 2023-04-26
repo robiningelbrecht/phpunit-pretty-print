a108c0a2c12a2cb19e2d6b5b20e4badeebb477f4
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2023-04-26 09:11:47 +0200

    Better tests

--- tests/ExampleTests/TestThatHasAllStatusesTest.php
+++ tests/ExampleTests/TestThatHasAllStatusesTest.php
@@ -11,21 +11,25 @@ class TestThatHasAllStatusesTest extends TestCase
 
     private const SLEEP_IN_MICRO_SECONDS = 10000;
 
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        usleep(self::SLEEP_IN_MICRO_SECONDS);
+    }
+
     public function testSuccess(): void
     {
-        usleep(self::SLEEP_IN_MICRO_SECONDS);
         $this->assertTrue(true);
     }
 
     public function testFail(): void
     {
-        usleep(self::SLEEP_IN_MICRO_SECONDS);
         $this->assertTrue(false);
     }
 
     public function testFailWithDiff(): void
     {
-        usleep(self::SLEEP_IN_MICRO_SECONDS);
         $this->assertEquals(
             ['one', 'two'],
             ['two', 'one']
@@ -34,24 +38,20 @@ class TestThatHasAllStatusesTest extends TestCase
 
     public function testError(): void
     {
-        usleep(self::SLEEP_IN_MICRO_SECONDS);
         throw new \Exception('error');
     }
 
     public function testRisky(): void
     {
-        usleep(self::SLEEP_IN_MICRO_SECONDS);
     }
 
     public function testSkip(): void
     {
-        usleep(self::SLEEP_IN_MICRO_SECONDS);
         $this->markTestSkipped('skipped');
     }
 
     public function testIncomplete(): void
     {
-        usleep(self::SLEEP_IN_MICRO_SECONDS);
         $this->markTestIncomplete('incomplete');
     }
 }
